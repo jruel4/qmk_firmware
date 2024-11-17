d1cd1dce437f3da741bc2a7a0bb9efb374bcf0b9
John Ruelas <jruel4@yahoo.com>
2024-11-16 20:11:07 -0600

    Add reset_keyboard functionality and shift+bksp = delete

--- keyboards/keychron/k2/rgb/v2/ansi/keymaps/dvorak_programmer_jruel4/keymap.c
+++ keyboards/keychron/k2/rgb/v2/ansi/keymaps/dvorak_programmer_jruel4/keymap.c
@@ -249,6 +249,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+uint8_t mod_state;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Store the current modifier state in the variable for later reference
+    mod_state = get_mods();
+    switch (keycode) {
+        case RGB_MOD:
+            {
+                if (record->event.pressed && ((get_mods() & (MOD_MASK_CTRL | MOD_MASK_SHIFT)) != 0)) {
+                  reset_keyboard();
+                }
+            }
+        case KC_BSPC:
+            {
+            // Initialize a boolean variable that keeps track
+            // of the delete key status: registered or not?
+            static bool delkey_registered;
+            if (record->event.pressed) {
+                // Detect the activation of either shift keys
+                if (mod_state & MOD_MASK_SHIFT) {
+                    // First temporarily canceling both shifts so that
+                    // shift isn't applied to the KC_DEL keycode
+                    del_mods(MOD_MASK_SHIFT);
+                    register_code(KC_DEL);
+                    // Update the boolean variable to reflect the status of KC_DEL
+                    delkey_registered = true;
+                    // Reapplying modifier state so that the held shift key(s)
+                    // still work even after having tapped the Backspace/Delete key.
+                    set_mods(mod_state);
+                    return false;
+                }
+            } else { // on release of KC_BSPC
+                // In case KC_DEL is still being sent even after the release of KC_BSPC
+                if (delkey_registered) {
+                    unregister_code(KC_DEL);
+                    delkey_registered = false;
+                    return false;
+                }
+            }
+            // Let QMK process the KC_BSPC keycode as usual outside of shift
+            return true;
+            }
+
+    }
+    return true;
+};
+
 bool dip_switch_update_user(uint8_t index, bool active) {
   switch(index) {
     case 0: // OS switch
