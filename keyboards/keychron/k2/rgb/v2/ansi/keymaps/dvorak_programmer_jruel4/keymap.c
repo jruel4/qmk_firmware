48240bf44a97e342ea55be4291013e233155e5a7
John Ruelas <jruel4@yahoo.com>
2024-11-16 20:15:41 -0600

    Fix mac keymap

--- keyboards/keychron/k2/rgb/v2/ansi/keymaps/dvorak_programmer_jruel4/keymap.c
+++ keyboards/keychron/k2/rgb/v2/ansi/keymaps/dvorak_programmer_jruel4/keymap.c
@@ -111,10 +111,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DVORAK_MAC_BASE] = LAYOUT_ansi(
   /*  0           1           2           3           4           5           6           7           8           9           10          11          12          13          14          15       */
       KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_MSCR,    KC_DEL,     RGB_MOD  ,
-      KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                KC_PGUP  ,
-      KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                KC_PGDN  ,
-      KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                KC_ENT,                 KC_HOME  ,
-      KC_LSFT,                KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSFT,    KC_UP,      KC_END   ,
+      KC_GRV,     KC_SLSH,    KC_7,       KC_5,       KC_3,       KC_1,       KC_9,       KC_0,       KC_2,       KC_4,       KC_6,       KC_8,       KC_EQL,     KC_BSPC,                KC_PGUP  ,
+      KC_TAB,     KC_SCLN,    KC_Y,       KC_O,       KC_P,       KC_COMM,    KC_F,       KC_G,       KC_C,       KC_R,       KC_L,       KC_LBRC,    KC_RBRC,    KC_BSLS,                KC_PGDN  ,
+      KC_CAPS,    KC_A,       KC_I,       KC_E,       KC_U,       KC_DOT,     KC_D,       KC_H,       KC_T,       KC_N,       KC_S,       KC_MINS,                KC_ENT,                 KC_HOME  ,
+      KC_LSFT,                KC_QUOT,    KC_Q,       KC_J,       KC_K,       KC_X,       KC_B,       KC_M,       KC_W,       KC_V,       KC_Z,                   KC_RSFT,    KC_UP,      KC_END   ,
       KC_LCTL,    KC_LALT,    KC_LGUI,                                        KC_SPC,                                         KC_RGUI,    MO(DM_FN),  KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT
   ),
 
